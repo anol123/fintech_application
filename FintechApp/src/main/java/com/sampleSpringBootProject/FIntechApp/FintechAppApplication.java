abbdc504dcc7fb946418fb83bb765953c46528b6
anol123 <anol.roy123@gmail.com>
2024-11-28 22:48:50 +0530

    set up  the correct folders and sdk

--- FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/FintechAppApplication.java
+++ FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/FintechAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FIntechAppApplication {
+public class FintechAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FIntechAppApplication.class, args);
+		SpringApplication.run(FintechAppApplication.class, args);
 	}
 
 }
