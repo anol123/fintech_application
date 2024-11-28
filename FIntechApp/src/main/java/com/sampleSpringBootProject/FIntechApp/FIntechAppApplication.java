b43663c139973c200531cda08157b795aedaef39
anol123 <anol.roy123@gmail.com>
2024-11-28 22:45:49 +0530

    first commit

---
+++ FIntechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/FIntechAppApplication.java
@@ -0,0 +1,13 @@
+package com.sampleSpringBootProject.FIntechApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIntechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FIntechAppApplication.class, args);
+	}
+
+}
