3b36c6d7c87a7b2389edf499bd688d6816348d9e
anol123 <anol.roy123@gmail.com>
2024-11-30 17:01:54 +0530

    Added the service layer

--- FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/service/BudgetService.java
+++ FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/service/BudgetService.java
@@ -1,4 +1,18 @@
 package com.sampleSpringBootProject.FIntechApp.service;
 
+import com.sampleSpringBootProject.FIntechApp.model.Budget;
+import com.sampleSpringBootProject.FIntechApp.repository.BudgetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BudgetService {
+    @Autowired
+    private BudgetRepository budgetRepository;
+
+    public List<Budget> getBudgetsByUserId(Long userId) {
+        return budgetRepository.findByUserId(userId);
+    }
 }
--- FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/service/UserService.java
+++ FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/service/UserService.java
@@ -1,4 +1,17 @@
 package com.sampleSpringBootProject.FIntechApp.service;
 
+import com.sampleSpringBootProject.FIntechApp.model.User;
+import com.sampleSpringBootProject.FIntechApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        // Add password hashing here (e.g., BCryptPasswordEncoder)
+        return userRepository.save(user);
+    }
 }
