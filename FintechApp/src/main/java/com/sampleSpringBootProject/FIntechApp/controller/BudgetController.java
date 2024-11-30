9aa815aa3334acb5c25fcc088b48814c54fcd2d0
anol123 <anol.roy123@gmail.com>
2024-11-30 17:07:58 +0530

    Added the controller layer

--- FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/controller/BudgetController.java
+++ FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/controller/BudgetController.java
@@ -1,4 +1,21 @@
 package com.sampleSpringBootProject.FIntechApp.controller;
 
+import com.sampleSpringBootProject.FIntechApp.model.Budget;
+import com.sampleSpringBootProject.FIntechApp.service.BudgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BudgetController {
+    @Autowired
+    private BudgetService budgetService;
+
+    @GetMapping("/{userId}")
+    public List<Budget> getBudgets(@PathVariable Long userId) {
+        return budgetService.getBudgetsByUserId(userId);
+    }
 }
