4a501aa1dfba4d89b86f43bacbad7288224b7f93
anol123 <anol.roy123@gmail.com>
2024-11-30 16:46:43 +0530

    Added Repository layer

--- FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/repository/BudgetRepository.java
+++ FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/repository/BudgetRepository.java
@@ -1,4 +1,10 @@
 package com.sampleSpringBootProject.FIntechApp.repository;
 
-public class BudgetRepository {
+import com.sampleSpringBootProject.FIntechApp.model.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BudgetRepository extends JpaRepository<Budget, Long> {
+    List<Budget> findByUserId(Long userId);
 }
--- FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/repository/UserRepository.java
+++ FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.sampleSpringBootProject.FIntechApp.repository;
 
-public class UserRepository {
+import com.sampleSpringBootProject.FIntechApp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
