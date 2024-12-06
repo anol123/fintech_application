3d04bc7364ab308152979373ba3fc935f3558162
anol123 <anol.roy123@gmail.com>
2024-12-06 21:25:05 +0530

    Added new User Service

--- FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/service/UserService.java
+++ FintechApp/src/main/java/com/sampleSpringBootProject/FIntechApp/service/UserService.java
@@ -5,13 +5,51 @@ import com.sampleSpringBootProject.FIntechApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+//@Service
+//public class UserService {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    public User registerUser(User user) {
+//        // Add password hashing here (e.g., BCryptPasswordEncoder)
+//        return userRepository.save(user);
+//    }
+//}
+
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
 
-    public User registerUser(User user) {
-        // Add password hashing here (e.g., BCryptPasswordEncoder)
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        return userRepository.findById(id).map(user -> {
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new RuntimeException("User not found with id " + id));
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
     }
 }
